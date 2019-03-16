56818aaefc8fa74f4ffba4285cb08f5b5a59b804
Jonathan Wong <jonwong@outlook.com>
2019-03-16 15:16:04 -0700

    Added new quote.

--- Marathoner/Data Layer/QuoteStore.swift
+++ Marathoner/Data Layer/QuoteStore.swift
@@ -23,7 +23,8 @@ final class QuoteStore {
         "See you at the finish line!",
         "You can do it!",
         "One step at a time!",
-        "One mile at a time!"
+        "One mile at a time!",
+        "Keep up the good work!"
     ]
 
     class func quote() -> String {
