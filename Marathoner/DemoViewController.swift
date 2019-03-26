60555d31adc6cca0fd907b8e6fd9136159202b5f
Jonathan Wong <jonwong@outlook.com>
2019-03-25 21:10:06 -0700

    DemoViewController added for code snippet demo.

---
+++ Marathoner/DemoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DemoViewController.swift
+//  Marathoner
+//
+//  Created by Jonathan Wong on 3/22/19.
+//  Copyright © 2019 Jonathan Wong. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
